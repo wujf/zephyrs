7c5b6d59336e3c79b14e5b5e17ef86bc0683ce24
Jianfeng Wu <wujf516@gmail.com>
2014-12-10 06:02:25 +0800

    fix typo

--- zephyrs.py
+++ zephyrs.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        slef.response.write('<html>hello,world</html>')
+        self.response.write('<html>hello,world</html>')
 
 application = webapp2.WSGIApplication([('/', MainPage)], debug=True)
 
